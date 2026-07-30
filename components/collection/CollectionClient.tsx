26a82535693210d01d2e8de5a7d75b25a9ee423b
Peter Murasky Jr <peter.murasky@gmail.com>
2026-07-30 09:34:47 -0400

    fix(build): add missing 'use client' directive to CollectionClient

--- components/collection/CollectionClient.tsx
+++ components/collection/CollectionClient.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState, useEffect } from 'react';
 import { useUser } from '@auth0/nextjs-auth0/client';
 import { useCollectionStore, CollectionItem } from '@/lib/store/useCollectionStore';
